8e9969ee497329d40ab1fe2bfdb23646541cbd77
Freddy <manyinyire2@gmail.com>
2026-01-20 15:00:08 +0200

    44

--- src/app/superadmin/layout.tsx
+++ src/app/superadmin/layout.tsx
@@ -32,52 +32,54 @@ export const metadata: Metadata = {
 
 export default function SuperAdminLayout({ children }: PropsWithChildren) {
   return (
-    <>
-      {/* Google Analytics */}
-      {GA_MEASUREMENT_ID && (
-        <>
-          <Script
-            strategy="afterInteractive"
-            src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
-          />
-          <Script
-            id="google-analytics-superadmin"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${GA_MEASUREMENT_ID}', {
-                  page_path: window.location.pathname,
-                });
-              `,
-            }}
-          />
-        </>
-      )}
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        {/* Google Analytics */}
+        {GA_MEASUREMENT_ID && (
+          <>
+            <Script
+              strategy="afterInteractive"
+              src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+            />
+            <Script
+              id="google-analytics-superadmin"
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${GA_MEASUREMENT_ID}', {
+                    page_path: window.location.pathname,
+                  });
+                `,
+              }}
+            />
+          </>
+        )}
 
-      <NextTopLoader color="#047857" showSpinner={false} />
-      <AnalyticsTracker />
+        <NextTopLoader color="#047857" showSpinner={false} />
+        <AnalyticsTracker />
 
-      <ToastProvider>
-        <SidebarProvider>
-          <div className="flex min-h-screen bg-atmospheric bg-gray-50 dark:bg-gray-900">
-            <SuperAdminSidebar />
+        <ToastProvider>
+          <SidebarProvider>
+            <div className="flex min-h-screen bg-atmospheric bg-gray-50 dark:bg-gray-900">
+              <SuperAdminSidebar />
 
-            <div className="w-full bg-white bg-organic-texture dark:bg-gray-800">
-              <ImpersonationBanner />
-              <SuperAdminHeader />
+              <div className="w-full bg-white bg-organic-texture dark:bg-gray-800">
+                <ImpersonationBanner />
+                <SuperAdminHeader />
 
-              <main className="isolate mx-auto w-full max-w-screen-2xl overflow-hidden p-4 md:p-6 2xl:p-10">
-                <div className="stagger-fade-in">
-                  {children}
-                </div>
-              </main>
+                <main className="isolate mx-auto w-full max-w-screen-2xl overflow-hidden p-4 md:p-6 2xl:p-10">
+                  <div className="stagger-fade-in">
+                    {children}
+                  </div>
+                </main>
+              </div>
             </div>
-          </div>
-        </SidebarProvider>
-      </ToastProvider>
-    </>
+          </SidebarProvider>
+        </ToastProvider>
+      </body>
+    </html>
   );
 }
